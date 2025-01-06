84fb5588875dba38fbcd80839e9379678e17d87b
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2025-01-06 23:18:38 +0500

    feat : install amqplib package

--- ride/app.js
+++ ride/app.js
@@ -3,14 +3,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 import dbconnect from './db/db.js'
 dbconnect()
-import userRoutes  from './routes/ride.routes.js'
+import rideRoutes  from './routes/ride.routes.js'
 import cookieParser  from 'cookie-parser'
 const app =express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use('/', userRoutes)
+app.use('/', rideRoutes)
 
 
 export default app
